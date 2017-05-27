9f2407f5c197cf31ac66ff54b62ca4ac70e53c90
Thorbiörn Fritzon <tfritzon@gmail.com>
2017-05-27 18:37:46 +0200

    Moved quote to a more convenient position.

--- keyboards/planck/keymaps/tallum/keymap.c
+++ keyboards/planck/keymaps/tallum/keymap.c
@@ -53,17 +53,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | RCtr |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |   -  |
+ * | LShft|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |   -  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   \  | RAlt | GUI  | LAlt | Symb |    Space    | Move | GUI  | RAlt | LAlt |   "  |
+ * |   \  | RAlt | GUI  | LAlt | Symb |    Space    | Move | GUI  | RAlt | RShft|   "  |
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = {
   {KC_TAB,     KC_Q,    KC_W,    KC_E,  KC_R,   KC_T,   KC_Y,  KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC},
   {KC_RCTL,    KC_A,    KC_S,    KC_D,  KC_F,   KC_G,   KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN, KC_ENT },
-  {KC_LSPO,    KC_Z,    KC_X,    KC_C,  KC_V,   KC_B,   KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_MINS},
-  /*{   FUNC, KC_RALT, KC_LGUI, KC_LALT,  SYMB, KC_SPC, KC_SPC,  MOVE, KC_RGUI, KC_RALT, KC_LALT, KC_QUOT}*/
-  {KC_BSLS, KC_RALT, MT(MOD_LGUI, KC_LBRACKET), TD(CT_LAC),  SYMB, KC_SPC, KC_SPC,  MOVE, TD(CT_RAC), MT(MOD_RGUI, KC_RBRACKET), KC_RSPC, KC_QUOT}
+  {KC_LSPO,    KC_Z,    KC_X,    KC_C,  KC_V,   KC_B,   KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_QUOT},
+  {KC_BSLS, KC_RALT, MT(MOD_LGUI, KC_LBRACKET), TD(CT_LAC),  SYMB, KC_SPC, KC_SPC,  MOVE, TD(CT_RAC), MT(MOD_RGUI, KC_RBRACKET), KC_RSPC, KC_MINS}
 },
 
 /* LANG:SWE
@@ -81,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LANG] = {
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SCLN, KC_QUOT},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SCLN, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SLSH}
 },
 */
@@ -116,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------------------------------.
  * |  Del |   &  |   *  |   ~  |   {  |   }  |   =  |   4  |   5  |   6  |   +  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   (  |   )  |   `  |   [  |   ]  |  0   |   1  |   2  |   3  |   \  |   |  |
+ * |      |   (  |   )  |   `  |   [  |   ]  |   0  |   1  |   2  |   3  |   \  |   |  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
